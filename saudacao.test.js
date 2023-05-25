6ed60ed3ad948a2ff20f28b27173a5ac263ff003
Wendel Gomes <wendelgomesdh7@gmail.com>
2023-05-25 17:56:53 +0200

    Primeiro commit

--- saudacao.test.js
+++ saudacao.test.js
@@ -2,8 +2,6 @@ const saudacao = require('./saudacao')
 
 describe('teste para saudacao'. () => {
     test('teste deverar retornar Olá, Maria', () => {
-        const olaMaria = saudacao.dizOla('Maria')
-        expect(olaMaria).toBe('Olá, Maria')
-        expect(true).toBeTruthy();
+        expect(saudacao('Maria')).toBe('Olá, Maria')
     })
 })
